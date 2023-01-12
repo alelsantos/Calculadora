a6f7b0485d2790f66c94b9abbaaa2cc166dec951
Alexandre Leite <51809504+alelsantos@users.noreply.github.com>
2023-01-12 19:21:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let resultados = document.querySelector("#resultados");
 let pesomil = document.querySelector("#pesomil");
 let pesototal = document.querySelector("#pesototal");
 
+const inputs = document.querySelectorAll("input");
+inputs.forEach(input => {
+    input.addEventListener('change', function(){
+        this.value = parseFloat(this.value).toFixed(2);
+    });
+});
+
+
 btn.addEventListener('click', () => {
     var largura = document.getElementById("largura").value;
     var altura = document.getElementById("altura").value;
@@ -14,25 +22,41 @@ btn.addEventListener('click', () => {
     var cores = document.getElementById("cores").value;
 
     var materialOption = material.options[material.selectedIndex].text;
-    var materialSplit = materialOption.split("-");
-    var materialValor = materialSplit[1];
+    if (materialOption == "PEBD"){
+        materialValor = 0.92
+    } else if (materialOption == "PEAD"){
+        materialValor = 0.95
+    } else if (materialOption == "PP"){
+        materialValor = 0.90
+    } else if (materialOption == "BOPP TRANSP"){
+        materialValor = 0.91
+    } else if (materialOption == "BOPP PEROLA"){
+        materialValor = 0.70
+    } else if (materialOption == "NYLON"){
+        materialValor = 1.0
+    }
     var pesoMil = largura*altura*espessura*fator*materialValor;
     var pesoTotal = pesoMil*quantidade;
     if (pesoTotal < 100 && modelo == 1 || pesoTotal < 100 && modelo == 0 && cores <= 4){
-        alert("Peso total não atingiu o minimo de 100");
+        alert("Peso total não atingiu o minimo de 100 kg");
         resultados.value = 0;
-        pesomil.value = 0;
-        pesototal.value = 0;
+        /*pesomil.value = 0;
+        pesototal.value = 0;*/
+    } else if(pesoTotal > 100 && modelo == 1 || pesoTotal > 100 && modelo == 0 && cores <= 4){
+        var qtdmin = 100/pesoMil;
+        resultados.value = parseFloat(qtdmin).toFixed(3);
+       /* pesomil.value = pesoMil;
+        pesototal.value = pesoTotal;*/
     } else if(pesoTotal < 300 && modelo == 0 && cores <= 6){
-        alert("Peso total não atingiu o minimo de 300");
+        alert("Peso total não atingiu o minimo de 300 kg");
         resultados.value = 0;
-        pesomil.value = 0;
-        pesototal.value = 0;
-    } else{
-        var qtdmin = pesoTotal/pesoMil;
-        resultados.value = qtdmin;
-        pesomil.value = pesoMil;
-        pesototal.value = pesoTotal;
+        /*pesomil.value = 0;
+        pesototal.value = 0;*/
+    } else if(pesoTotal > 300 && modelo == 0 && cores <= 6){
+        var qtdmin = 300/pesoMil;
+        resultados.value = parseFloat(qtdmin).toFixed(3);
+        /*pesomil.value = pesoMil;
+        pesototal.value = pesoTotal;*/
         /*alert(pesoMil);
         alert(pesoTotal);
         alert(qtdmin);*/
@@ -45,9 +69,11 @@ function colors(){
     if (modelo == 1){
         cores.readOnly = true;
         cores.style.pointerEvents = "none";
-        cores.value = '1';
+        cores.innerHTML += `<option>N/A</option>`
+        cores.value = 'N/A';
     }else{
         cores.readOnly = false;
         cores.style.pointerEvents = "fill";
+        cores.removeChild(cores.lastElementChild);
     };
 };
